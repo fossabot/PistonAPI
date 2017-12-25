03ea7590ad6576b9a1f99d14438106a95064ef46
Harry Fox <harry.fox.ic@icloud.com>
2017-12-25 21:56:26 +0000

    Move ProtocolState to API and add direction

---
+++ src/main/java/org/laxio/piston/piston/protocol/PacketDirection.java
@@ -0,0 +1,11 @@
+package org.laxio.piston.piston.protocol;
+
+/**
+ * Represents the direction which a packet is intended to travel
+ */
+public enum PacketDirection {
+
+    SERVERBOUND,
+    CLIENTBOUND
+
+}
