6c5b566d1f7e0df7c7faa2afdebc056205dfd762
Harry Fox <harry.fox.ic@icloud.com>
2017-12-31 15:07:28 +0000

    Add support for multiple protocols in handshake

--- src/main/java/org/laxio/piston/piston/translator/ProtocolTranslator.java
+++ src/main/java/org/laxio/piston/piston/translator/ProtocolTranslator.java
@@ -1,12 +1,18 @@
 package org.laxio.piston.piston.translator;
 
+import org.laxio.piston.piston.PistonServer;
 import org.laxio.piston.piston.protocol.Packet;
+import org.laxio.piston.piston.versioning.Version;
 
 public interface ProtocolTranslator {
 
-    int getTranslatedVersion();
+    PistonServer getServer();
+
+    Version getNativeMinecraftVersion();
 
     int getNativeVersion();
+
+    int getTranslatedVersion();
 
     boolean matches(Packet packet);
 
